1af2a8b30caae3da15e20cbff9884568304109b3
Gabriel-Lucena <gabriel.lucena.cunha@hotmail.com>
2021-09-23 10:41:07 -0300

    Página logout

--- aula07/area_restrita.php
+++ aula07/area_restrita.php
@@ -35,12 +35,31 @@ verificarLogin();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+
     <title>ÁREA RESTRISTA</title>
 </head>
 
 <body>
 
-    <h1>Tabela</h1>
+    <h1>ÁREA RESTRITA</h1>
+
+    <div class='toolbar'>
+
+        <h2>
+
+            <?php echo 'Olá ' . strtoupper($_SESSION['usuario']) . ' - Login efetutado em: ' . $_SESSION['data_hora']; ?>
+
+        </h2>
+
+        <h2>
+
+            <a class="material-icons" href="processa_login.php?logout=true">logout</a>
+
+        </h2>
+
+    </div>
 
 </body>
 
