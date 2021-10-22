9c1796d9ee397795883a220080cca483f3e3546e
Gabriel-Lucena <gabriel.lucena.cunha@hotmail.com>
2021-10-22 11:24:22 -0300

    Campos de login

--- aula08/componentes/header/header.php
+++ aula08/componentes/header/header.php
@@ -14,4 +14,83 @@ $raiz = "/Gabriel/aula_php/aula08";
         </a>
     </figure>
 
+
+    <?php
+
+    if (!isset($_SESSION["usuarioId"])) {
+    ?>
+        <nav>
+            <ul>
+                <a id="menu-admin">Administrar</a>
+            </ul>
+        </nav>
+
+        <div id="container-login" class="container-login">
+
+            <h1>Fazer Login</h1>
+
+            <form method="POST" action="/componentes/header/acoesLogin.php">
+                <input type="hidden" name="acao" value="login" />
+                <input type="text" name="usuario" placeholder="Usuário" />
+                <input type="password" name="senha" placeholder="Senha" />
+                <button>Entrar</button>
+            </form>
+
+        </div>
+
+    <?php
+
+    } else {
+
+    ?>
+        <nav>
+            <ul>
+                <a id="menu-admin" onclick="logout()">Sair</a>
+            </ul>
+        </nav>
+
+        <form id="form-logout" style="display:none" method="POST" action="/componentes/header/acoesLogin.php">
+            <input type="hidden" name="acao" value="logout" />
+        </form>
+
+    <?php
+    }
+    ?>
+
 </header>
+
+
+<script lang="javascript">
+    document.querySelector("#menu-admin").addEventListener("click", toggleLogin);
+
+    function logout() {
+
+        document.querySelector("#form-logout").submit();
+
+    }
+
+    function toggleLogin() {
+
+        let containerLogin = document.querySelector("#container-login");
+        let h1Form = document.querySelector("#container-login > h1");
+        let form = document.querySelector("#container-login > form");
+        //se estiver oculto, mostra 
+
+        if (containerLogin.style.opacity == 0) {
+            h1Form.style.display = "block";
+            form.style.display = "flex";
+            containerLogin.style.opacity = 1;
+            containerLogin.style.height = "200px";
+            //se não, oculta
+
+        } else {
+
+            h1Form.style.display = "none";
+            form.style.display = "none";
+            containerLogin.style.opacity = 0;
+            containerLogin.style.height = "0px";
+
+        }
+    }
+    
+</script>
